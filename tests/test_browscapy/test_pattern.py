72dc308084aa72269e0362f9cfa78407384c8eb7
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2017-04-10 13:18:53 -0300

    Prepare Pattern class for pickling

--- tests/test_browscapy/test_pattern.py
+++ tests/test_browscapy/test_pattern.py
@@ -8,11 +8,13 @@ class TestPattern(unittest.TestCase):
     def test_conversion_to_python(self):
         bcap_pat = '(.?*+)'
         expected = r'^\(\...*?\+\)$'
-        actual = Pattern.to_python(bcap_pat)
+        pattern = Pattern(bcap_pat)
+        actual = pattern.compiled.pattern
         self.assertEqual(actual, expected)
 
     def test_length(self):
         bcap_pat = '(.?*+)'
         expected = 4  # only * and ? are symbols
-        actual = Pattern.get_length(bcap_pat)
+        pattern = Pattern(bcap_pat)
+        actual = pattern.length
         self.assertEqual(actual, expected)
